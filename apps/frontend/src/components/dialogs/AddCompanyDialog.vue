7d8382d0f454426c919683c7792ad3bd57b4486f
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-03-26 08:06:12 +0100

    implemented api communication for addCompanyDialog, for company and address creation

--- apps/frontend/src/components/dialogs/AddCompanyDialog.vue
+++ apps/frontend/src/components/dialogs/AddCompanyDialog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Dialog, Button, InputText, Message, FloatLabel } from "primevue";
-import { Form, FormField } from "@primevue/forms";
+import { Form, FormField, type FormSubmitEvent } from "@primevue/forms";
 
 import { zodResolver } from "@primevue/forms/resolvers/zod";
 import { z } from "zod";
@@ -9,11 +9,21 @@ import { useToast } from "primevue/usetoast";
 import AppLogo from "../AppLogo.vue";
 import { reactive } from "vue";
 
+import { createCompany } from "../../lib/services/companyService";
+import { createAddress } from "../../lib/services/addressService";
+
+const props = defineProps<{
+  user: string;
+  visible: boolean;
+}>();
+
 const toast = useToast();
 
 const initialValues = reactive({
   name: "",
   nip: "",
+  krs: "",
+  regon: "",
   street_and_number: "",
   zipcode: "",
   city: "",
@@ -25,24 +35,26 @@ const resolver = zodResolver(
       .string()
       .min(1, { message: "Nazwa firmy jest wymagana." })
       .min(3, { message: "Nazwa musi mieć min. 3 znaki." }),
-
     nip: z
       .string()
       .min(1, { message: "NIP jest wymagany." })
       .regex(/^\d{10}$/, { message: "NIP musi mieć dokładnie 10 cyfr." }),
-
+    krs: z.string().refine((val) => val === "" || /^\d{10}$/.test(val), {
+      message: "KRS musi mieć dokładnie 10 cyfr lub pozostać pusty",
+    }),
+    regon: z.string().refine((val) => val === "" || /^(\d{9}|\d{14})$/.test(val), {
+      message: "REGON musi mieć 9 lub 14 cyfr lub pozostać pusty",
+    }),
     street_and_number: z
       .string()
       .min(1, { message: "Adres jest wymagany." })
       .min(5, { message: "Podaj pełny adres (ulica i numer)." }),
-
     zipcode: z
       .string()
       .min(1, { message: "Kod pocztowy jest wymagany." })
       .regex(/^\d{2}-\d{3}$/, {
         message: "Kod pocztowy musi być w formacie XX-XXX.",
       }),
-
     city: z
       .string()
       .min(1, { message: "Miasto jest wymagane." })
@@ -50,9 +62,29 @@ const resolver = zodResolver(
   }),
 );
 
-const onFormSubmit = ({ valid }) => {
-  if (valid) {
-    toast.add({ severity: "success", summary: "Form is submitted.", life: 3000 });
+const onFormSubmit = async (event: FormSubmitEvent) => {
+  const { valid } = event;
+  if (!valid) return;
+
+  try {
+    const companyResp = await createCompany({
+      userId: 1,
+      name: event.values.name,
+      nip: event.values.nip,
+      krs: event.values.krs,
+      regon: event.values.regon,
+    });
+
+    const addressResp = await createAddress({
+      companyId: companyResp.companyId,
+      street_and_number: event.values.street_and_number,
+      zipcode: event.values.zipcode,
+      city: event.values.city,
+    });
+
+    toast.add({ severity: "success", summary: "Firma i adres dodane pomyślnie!", life: 3000 });
+  } catch (err) {
+    toast.add({ severity: "error", summary: "Coś poszło nie tak", life: 3000 });
   }
 };
 
@@ -61,7 +93,7 @@ const emit = defineEmits(["update:visible"]);
 
 <template>
   <Dialog
-    :visible="true"
+    :visible="props.visible"
     @update:visible="emit('update:visible', $event)"
     modal
     :closable="false"
@@ -71,9 +103,7 @@ const emit = defineEmits(["update:visible"]);
     <template #header>
       <AppLogo />
     </template>
-    <span class="block mb-8 font-semibold">{{
-      "Dodaj firmę do użytkownika: Łukasz Kowalski"
-    }}</span>
+    <span class="block mb-5 font-semibold"> Dodaj firmę użytkownika: {{ props.user }} </span>
     <div class="card flex justify-center">
       <Form
         :initialValues
@@ -94,6 +124,24 @@ const emit = defineEmits(["update:visible"]);
           <FloatLabel variant="on">
             <InputText v-bind="$field.props" id="nip_input" type="text" fluid />
             <label for="nip_input">NIP Firmy</label>
+          </FloatLabel>
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+        </FormField>
+        <FormField v-slot="$field" name="krs" class="flex flex-col gap-1">
+          <FloatLabel variant="on">
+            <InputText v-bind="$field.props" id="krs_input" type="text" fluid />
+            <label for="krs_input">KRS Firmy (Opcjonalnie)</label>
+          </FloatLabel>
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+        </FormField>
+        <FormField v-slot="$field" name="regon" class="flex flex-col gap-1">
+          <FloatLabel variant="on">
+            <InputText v-bind="$field.props" id="regon_input" type="text" fluid />
+            <label for="regon_input">REGON Firmy (Opcjonalnie)</label>
           </FloatLabel>
           <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
             $field.error?.message
