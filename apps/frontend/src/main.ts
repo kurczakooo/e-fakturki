8f48195dbf8129e2733c7c67efb17cb3c3f9489d
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-03-27 22:58:02 +0100

    cleaned main.ts to only hold the minimum and import configs

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -3,54 +3,25 @@ import "./style.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import { createPinia } from "pinia";
 import { ToastService } from "primevue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
-
 import PrimeVue from "primevue/config";
-import Aura from "@primeuix/themes/aura";
+import { primeVueConfig } from "./config";
 import Tooltip from "primevue/tooltip";
-import { definePreset } from "@primeuix/themes";
-
-const customPreset = definePreset(Aura, {
-  //https://primevue.org/theming/styled/
-  semantic: {
-    primary: {
-      50: "{sky.50}",
-      100: "{sky.100}",
-      200: "{sky.200}",
-      300: "{sky.300}",
-      400: "{sky.400}",
-      500: "{sky.500}",
-      600: "{sky.600}",
-      700: "{sky.700}",
-      800: "{sky.800}",
-      900: "{sky.900}",
-      950: "{sky.950}",
-    },
-    colorScheme: {
-      light: {},
-      dark: {},
-    },
-  },
-});
 
 const app = createApp(App);
+const pinia = createPinia();
+
 app.use(router);
+
 app.use(ToastService);
+
 app.use(VueQueryPlugin);
-app.use(PrimeVue, {
-  theme: {
-    preset: customPreset,
-    options: {
-      prefix: "prime",
-      darkModeSelector: ".prime-dark-mode",
-      //   darkModeSelector: "system",
-    },
-  },
-  ripple: true, // Enable ripple effect for buttons globally
-  inputVariant: "filled", // Set default input variant
-});
+
+app.use(pinia);
+
+app.use(PrimeVue, primeVueConfig);
 app.directive("tooltip", Tooltip);
 
 app.mount("#app");
