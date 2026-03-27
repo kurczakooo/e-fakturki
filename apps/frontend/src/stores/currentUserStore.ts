c2bfde1c768526daaa7c95aedc72ee2598d11f0a
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-03-27 22:56:41 +0100

    created currentUserStore to hold basic currently logged in user data

---
+++ apps/frontend/src/stores/currentUserStore.ts
@@ -0,0 +1,35 @@
+import { defineStore } from "pinia";
+
+export const useCurrentUserStore = defineStore("currentUser", {
+  state: () => {
+    return {
+      userId: null as number | null,
+      name: null as string | null,
+      lastname: null as string | null,
+      email: null as string | null,
+
+      companyId: null as number | null,
+      companyName: null as string | null,
+    };
+  },
+  getters: {
+    getUserId: (state) => state.userId,
+    getName: (state) => state.name,
+    getLastname: (state) => state.lastname,
+    getEmail: (state) => state.email,
+    getCompanyId: (state) => state.companyId,
+    getCompanyName: (state) => state.companyName,
+  },
+  actions: {
+    setUserData(userId: number, name: string, lastname: string, email: string) {
+      this.userId = userId;
+      this.name = name;
+      this.lastname = lastname;
+      this.email = email;
+    },
+    setCompanyData(companyId: number, companyName: string) {
+      this.companyId = companyId;
+      this.companyName = companyName;
+    },
+  },
+});
