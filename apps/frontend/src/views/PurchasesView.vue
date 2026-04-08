c893e00c9dec9d9637468794dc464436c4fb574c
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-08 19:55:19 +0200

    added invoice table tailored in both invoice list views

--- apps/frontend/src/views/PurchasesView.vue
+++ apps/frontend/src/views/PurchasesView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InvoicesTable from "../components/invoices/InvoicesTable.vue";
+</script>
 
 <template>
-  {{ "Purchases" }}
+  <InvoicesTable invoice_type="purchase" />
 </template>
--- apps/frontend/src/views/SalesView.vue
+++ apps/frontend/src/views/SalesView.vue
@@ -3,5 +3,5 @@ import InvoicesTable from "../components/invoices/InvoicesTable.vue";
 </script>
 
 <template>
-  <InvoicesTable invoice_type="purchase" />
+  <InvoicesTable invoice_type="sales" />
 </template>
