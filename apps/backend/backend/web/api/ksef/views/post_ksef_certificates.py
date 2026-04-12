db2b0a98027d79afe31344bd2272fe3b88eb7143
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-12 21:33:20 +0200

    refactored post_ksef_certificates, deleted schema for arguments and docstring

--- apps/backend/backend/web/api/ksef/views/post_ksef_certificates.py
+++ apps/backend/backend/web/api/ksef/views/post_ksef_certificates.py
@@ -1,38 +1,32 @@
 from fastapi import APIRouter, Depends, File, Form, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from backend.web.api.ksef.schemas import CredentialsCreateResponse
 from backend.db.dependencies import get_db_session
 from backend.db.models.ksef_credentials import KsefCredentialsTable
-from backend.web.api.ksef.schemas import KsefCertificatesUploadStatus
 
 router = APIRouter()
 
 
-@router.post(
-    "/ksef/certificates", response_model=KsefCertificatesUploadStatus, status_code=201
-)
+@router.post("/certificates", status_code=201, response_model=CredentialsCreateResponse)
 async def upload_ksef_certificates(
-    company_id: int = Form(...),
-    certificates_for_auth: bool = Form(...),
-    certificate: UploadFile = File(...),
-    private_key: UploadFile = File(...),
-    password: str = Form(...),
+    company_id: int = Form(
+        ..., description="The ID of the company to which the certificates belong."
+    ),
+    certificates_for_auth: bool = Form(
+        ...,
+        description="Whether the uploaded certificates are for authentication (True) or for offline mode (False).",
+    ),
+    certificate: UploadFile = File(
+        ..., description="The certificate file to be uploaded."
+    ),
+    private_key: UploadFile = File(
+        ..., description="The private key file to be uploaded."
+    ),
+    password: str = Form(..., description="The password for the private key."),
     db_session: AsyncSession = Depends(get_db_session),
-) -> KsefCertificatesUploadStatus:
-    """
-    Uploads KSeF certificates for a company to the database.
-
-    Args:
-        company_id: The ID of the company.
-        certificates_for_auth: Whether the uploaded certificates are for authentication (True) or for offline mode (False).
-        certificate: The certificate file to be uploaded.
-        private_key: The private key file to be uploaded.
-        password: The password for the private key.
-        db_session: The database session.
-
-    Returns:
-        An instance of KsefCertificatesUploadStatus containing the ID of the uploaded certificates.
-    """
+) -> CredentialsCreateResponse:
+    """Uploads KSeF certificates for a company to the database."""
 
     # TODO encode the certs before saving to db, and decode when loading from db
     certificate_str = (await certificate.read()).decode("utf-8")
@@ -55,4 +49,6 @@ async def upload_ksef_certificates(
     db_session.add(credentials)
     await db_session.flush()
     await db_session.refresh(credentials)
-    return KsefCertificatesUploadStatus(ksef_certificates_id=int(credentials.id))
+    return CredentialsCreateResponse(
+        company_id=credentials.company_id, credentials_id=credentials.id
+    )
