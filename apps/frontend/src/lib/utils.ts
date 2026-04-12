d6885c2c1a94fd37593c5ad9fafefacbdbd5efd4
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-12 21:22:02 +0200

    created a function to format dates

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -1,3 +1,11 @@
 export function toggleDarkMode() {
   document.documentElement.classList.toggle("prime-dark-mode");
 }
+
+export function formatLocalDate(date: Date) {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
+  const dd = String(date.getDate()).padStart(2, "0");
+
+  return `${yyyy}-${mm}-${dd}`;
+}
