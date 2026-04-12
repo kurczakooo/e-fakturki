69375cc7da9528e09f7bbcf10024febbf0f8c19c
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-12 21:34:31 +0200

    created get_single_invoice endpoint to pull invoices xml from KSeF

---
+++ apps/backend/backend/web/api/ksef/views/get_single_invoice.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Depends, Form
+from ksef_client import KsefClient, KsefClientOptions
+from ksef_client.models import InvoiceContent
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.db.dependencies import get_db_session
+from backend.db.repositories.company_repository import get_company_nip
+from backend.services.ksef.auth.certificate_auth import (
+    authorize_ksef_with_certificate,
+    certificate_str_to_temp_file,
+    remove_temp_file,
+)
+from backend.services.ksef.auth.ksef_session import open_ksef_session
+from backend.web.api.ksef.services import get_auth_certs
+
+from backend.settings import Settings
+
+router = APIRouter()
+
+
+@router.post("/ksef/single_download", status_code=202)
+async def download_invoice_from_ksef(
+    company_id: int = Form(...),
+    ksef_reference_number: str = Form(...),
+    db_session: AsyncSession = Depends(get_db_session),
+) -> InvoiceContent:
+    """Download single invoice xml from KSeF."""
+
+    # load needed company data for the upload, including NIP and certs
+    company_nip = await get_company_nip(db_session, company_id)
+    company_ksef_certs = await get_auth_certs(db_session, company_id)
+    password = company_ksef_certs.password
+    cert_path = certificate_str_to_temp_file(company_ksef_certs.certificate)
+    key_path = certificate_str_to_temp_file(company_ksef_certs.private_key)
+
+    with KsefClient(KsefClientOptions(base_url=Settings.ksef_environment())) as client:
+        certs = client.security.get_public_key_certificates()
+        sym_cert = next(
+            c["certificate"]
+            for c in certs
+            if "SymmetricKeyEncryption" in (c.get("usage") or [])
+        )
+
+        # authenticate with certs
+        access_token = (
+            authorize_ksef_with_certificate(
+                certificate_path=cert_path,
+                private_key_path=key_path,
+                private_key_password=password,
+                client=client,
+                context_id_value=company_nip,
+            )
+        ).tokens.access_token.token
+
+        ksef_session_params = open_ksef_session(
+            client=client, access_token=access_token, sym_cert=sym_cert
+        )
+
+        invoice = client.invoices.get_invoice(
+            ksef_number=ksef_reference_number, access_token=access_token
+        )
+
+        ksef_session_params.workflow.close_session(
+            ksef_session_params.session.session_reference_number, access_token
+        )
+        remove_temp_file(cert_path)
+        remove_temp_file(key_path)
+
+        return invoice
