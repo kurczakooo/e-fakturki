4ccdf97b22775040e7404afa7dfaacf90d447141
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-13 18:17:51 +0200

    refactored users table db model

--- apps/backend/backend/db/models/users.py
+++ apps/backend/backend/db/models/users.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
-from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from sqlalchemy.sql import func
 
 from backend.db.base import Base
@@ -10,13 +12,17 @@ class UsersTable(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    email = Column(String(255), nullable=False, unique=True)
-    password = Column(String(255), nullable=False)
-    is_active = Column(Boolean, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), onupdate=func.now()
+    )
 
     companies = relationship("CompaniesTable", back_populates="owner")
