d7049bc44d8769cbf89ff5522ae8bda34fdded3c
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-15 15:07:29 +0200

    added a popup for user options on top bar, and implemented logout functionality

--- apps/frontend/src/components/TopBar.vue
+++ apps/frontend/src/components/TopBar.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
 import { computed } from "vue";
-
-import { Toolbar, Select, Button } from "primevue";
+import { ref } from "vue";
+import { Toolbar, Select, Button, Popover, useToast, Divider } from "primevue";
 
 import AppLogo from "./AppLogo.vue";
 import { useCurrentUserStore } from "../stores/currentUserStore";
+import { userLogout } from "../lib/services/authService";
+import router from "../router";
 
 const currentUserStore = useCurrentUserStore();
+const toast = useToast();
+
+const userPopup = ref();
+const toggleuserPopup = (event: any) => {
+  userPopup.value.toggle(event);
+};
+
+function logout() {
+  userLogout();
+  userPopup.value.hide();
+  router.push("/login");
+  setTimeout(() => {
+    toast.add({ severity: "info", summary: "Poprawnie wylogowano", life: 3000 });
+  }, 100);
+}
 
 const companyName = computed(() => currentUserStore.getCompanyName);
 const userName = computed(() => currentUserStore.getFullName);
@@ -25,9 +42,20 @@ const userName = computed(() => currentUserStore.getFullName);
           emptyMessage="Brak opcji do wyboru"
         />
         <p>{{ userName ? userName : "Zaloguj się" }}</p>
-        <Button icon="pi pi-user" raised rounded />
-        <Button icon="pi pi-cog" raised rounded />
+        <Button icon="pi pi-user" raised rounded @click="toggleuserPopup" />
       </div>
     </template>
   </Toolbar>
+
+  <!-- user popup -->
+  <Popover ref="userPopup">
+    <div class="flex flex-col w-40">
+      <div class="flex flex-col">
+        <span>Zalogowano jako:</span>
+        <span class="font-semibold">{{ currentUserStore.getFullName }}</span>
+      </div>
+      <Divider />
+      <Button icon="pi pi-on-off-button" label="Wyloguj" raised @click="logout"></Button>
+    </div>
+  </Popover>
 </template>
