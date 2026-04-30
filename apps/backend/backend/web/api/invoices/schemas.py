6c6fc84881a7418f2c30f99444418c71523d81c1
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-04-30 07:40:12 +0200

    created scheamas for invoice object to send to frontend

--- apps/backend/backend/web/api/invoices/schemas.py
+++ apps/backend/backend/web/api/invoices/schemas.py
@@ -1,6 +1,9 @@
-"""Schemas for invoices API."""
-
 from pydantic import BaseModel
+from datetime import datetime
+
+from backend.db.models.invoices_brief import KsefStatus
+from backend.db.repositories.invoices_payment_repository import InvoicePayment
+from backend.db.repositories.invoices_entries_repository import InvoiceEntry
 
 
 class PageInfo(BaseModel):
@@ -18,3 +21,48 @@ class InvoicesListResponse(BaseModel):
 
     invoices: list[dict]
     page_info: PageInfo
+
+
+class InvoiceCompanyData(BaseModel):
+    """Response model for invoice buyer and seller detals."""
+
+    name: str
+    nip: str
+    address_l1: str | None
+    address_l2: str | None
+    email: str | None
+    phone: str | None
+
+
+class InvoiceResponse(BaseModel):
+    """Response model for full invoice with details."""
+
+    id: str
+    invoice_number: str
+    invoice_type: str
+    ksef_number: str | None
+    invoicing_date: datetime | None
+    acquisition_date: datetime | None
+    permanent_storage_date: datetime | None
+    ksef_status: KsefStatus
+    is_new: bool
+    ###
+    issue_date: datetime
+    issue_place: str | None
+    ###
+    seller_info: InvoiceCompanyData
+    buyer_info: InvoiceCompanyData
+    ###
+    entries: list[InvoiceEntry]
+    ###
+    currency: str
+    net_total: float
+    tax_total: float
+    gross_total: float
+    ###
+    payment: InvoicePayment | None
+    ###
+    annotations: str | None
+    additional_info: str | None
+    footer_info: str | None
+    footer_registers: str | None
