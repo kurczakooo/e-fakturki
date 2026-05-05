d8de0a34470b5d465f4031a09b2a202302dc80d2
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-05-05 17:08:39 +0200

    refeactored addCompanyDialog to contain addrl1 and addrl2 instead of all address pieces normalized

--- apps/frontend/src/components/dialogs/AddCompanyDialog.vue
+++ apps/frontend/src/components/dialogs/AddCompanyDialog.vue
@@ -9,21 +9,19 @@ import { z } from "zod";
 
 import AppLogo from "../AppLogo.vue";
 import { createCompany } from "../../lib/services/companyService";
-import { createAddress } from "../../lib/services/addressService";
 import { useCurrentUserStore } from "../../stores/currentUserStore";
 
 const toast = useToast();
 const currentUserStore = useCurrentUserStore();
-
+const userName = computed(() => currentUserStore.getFullName);
 const initialValues = reactive({
   name: "",
   nip: "",
   krs: "",
   regon: "",
-  street: "",
-  number: "",
-  zipcode: "",
-  city: "",
+  addressL1: "",
+  addressL2: "",
+  email: "",
 });
 
 const resolver = zodResolver(
@@ -42,56 +40,50 @@ const resolver = zodResolver(
     regon: z.string().refine((val) => val === "" || /^(\d{9}|\d{14})$/.test(val), {
       message: "REGON musi mieć 9 lub 14 cyfr lub pozostać pusty",
     }),
-    street: z.string().min(4, { message: "Ulica jest wymagana." }),
-    number: z
-      .string()
-      .min(1, { message: "Numer jest wymagany." })
-      .max(10, { message: "Numer może mieć maksymalnie 10 znaków." })
-      .regex(/^[0-9A-Za-z\/]+$/, {
-        message: "Numer może zawierać tylko cyfry, litery i znak '/'.",
-      }),
-    zipcode: z
-      .string()
-      .min(1, { message: "Kod pocztowy jest wymagany." })
-      .regex(/^\d{2}-\d{3}$/, {
-        message: "Kod pocztowy musi być w formacie XX-XXX.",
-      }),
-    city: z
-      .string()
-      .min(1, { message: "Miasto jest wymagane." })
-      .min(2, { message: "Miasto musi mieć min. 2 znaki." }),
+    addressL1: z.string(),
+    addressL2: z.string(),
+    email: z.string(),
   }),
 );
 
-const createCompanyWithAddressMutation = useMutation({
+const createCompanyMutation = useMutation({
   mutationFn: async (values: any) => {
     const companyResp = await createCompany({
-      user_id: currentUserStore.getUserId,
+      owner_id: currentUserStore.getUserId,
       name: values.name,
       nip: values.nip,
       krs: values.krs == "" ? null : values.krs,
       regon: values.regon == "" ? null : values.regon,
+      country_code: "PL",
+      address_l1: values.addressL1 == "" ? null : values.addressL1,
+      address_l2: values.addressL2 == "" ? null : values.addressL2,
+      address_correspondance_l1: null,
+      address_correspondance_l2: null,
+      email: values.email == "" ? null : values.email,
+      phone_number: null,
+      additional_info: null,
     });
 
-    const addressResp = await createAddress({
-      company_id: companyResp.company_id,
-      type: "registered",
-      country: "Poland",
-      city: values.city,
-      postal_code: values.zipcode,
-      street: values.street,
-      building_number: values.number,
-    });
-    return { companyResp, addressResp };
+    return companyResp;
   },
 
   onSuccess: (data, variables) => {
-    toast.add({ severity: "success", summary: "Firma i adres dodane pomyślnie!", life: 3000 });
-    currentUserStore.setCompanyData(data.companyResp.company_id, variables.name, false);
+    toast.add({ severity: "success", summary: "Firma dodana pomyślnie!", life: 3000 });
+    currentUserStore.setCompanyData(
+      data.company_id,
+      variables.name,
+      variables.nip,
+      variables.country_code,
+      variables.address_l1,
+      variables.address_l2,
+      variables.email,
+      variables.phone_number,
+      false,
+    );
   },
 
-  onError: () => {
-    toast.add({ severity: "error", summary: "Coś poszło nie tak", life: 3000 });
+  onError: (error) => {
+    toast.add({ severity: "error", summary: error.response?.data?.detail, life: 3000 });
   },
 });
 
@@ -99,18 +91,19 @@ const onFormSubmit = async (event: FormSubmitEvent) => {
   const { valid } = event;
   if (!valid) return;
 
-  createCompanyWithAddressMutation.mutate(event.values);
+  createCompanyMutation.mutate(event.values);
 };
-
-const userName = computed(() => currentUserStore.getFullName);
 </script>
 
 <template>
-  <Dialog :visible="true" modal :closable="false" :draggable="false" :style="{ width: '40rem' }">
+  <Dialog modal :closable="false" :draggable="false" :style="{ width: '40rem' }">
     <template #header>
       <AppLogo />
     </template>
-    <span class="block mb-5 font-semibold"> Dodaj firmę użytkownika: {{ userName }} </span>
+    <span class="block mb-2 font-semibold"> Dodaj firmę użytkownika: {{ userName }} </span>
+    <span class="block mb-5 text-sm text-gray-500"
+      >Pola opcjonalne i dodatkowe informacje o firmie można zmienić później.</span
+    >
     <div class="card flex justify-center">
       <Form
         :initialValues
@@ -158,49 +151,28 @@ const userName = computed(() => currentUserStore.getFullName);
             </FormField>
           </div>
           <div class="flex flex-col gap-4 w-full">
-            <FormField v-slot="$field" name="street" class="flex flex-col gap-1">
+            <FormField v-slot="$field" name="addressL1" class="flex flex-col gap-1">
               <FloatLabel variant="on">
-                <InputText v-bind="$field.props" id="street_input" type="text" fluid />
-                <label for="street_input">Ulica</label>
+                <InputText v-bind="$field.props" id="addresL1_input" type="text" fluid />
+                <label for="addresL1_input">Adres linia 1 (Opcjonalnie)</label>
               </FloatLabel>
               <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
                 $field.error?.message
               }}</Message>
             </FormField>
-            <FormField v-slot="$field" name="number" class="flex flex-col gap-1">
+            <FormField v-slot="$field" name="addressL2" class="flex flex-col gap-1">
               <FloatLabel variant="on">
-                <InputText v-bind="$field.props" id="building_number_input" type="text" fluid />
-                <label for="building_number_input">Numer budynku</label>
+                <InputText v-bind="$field.props" id="addresL2_input" type="text" fluid />
+                <label for="addresL2_input">Adres linia 2 (Opcjonalnie)</label>
               </FloatLabel>
               <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
                 $field.error?.message
               }}</Message>
             </FormField>
-            <FormField v-slot="$field" name="zipcode" class="flex flex-col gap-1">
+            <FormField v-slot="$field" name="email" class="flex flex-col gap-1">
               <FloatLabel variant="on">
-                <InputText
-                  v-bind="$field.props"
-                  id="zipcode_input"
-                  type="text"
-                  :feedback="false"
-                  fluid
-                />
-                <label for="zipcode_input">Kod pocztowy</label>
-              </FloatLabel>
-              <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
-                $field.error?.message
-              }}</Message>
-            </FormField>
-            <FormField v-slot="$field" name="city" class="flex flex-col gap-1">
-              <FloatLabel variant="on">
-                <InputText
-                  v-bind="$field.props"
-                  id="city_input"
-                  type="text"
-                  :feedback="false"
-                  fluid
-                />
-                <label for="city_input">Miasto</label>
+                <InputText v-bind="$field.props" id="email_input" type="text" fluid />
+                <label for="email_input">E-mail (Opcjonalnie)</label>
               </FloatLabel>
               <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
                 $field.error?.message
@@ -212,8 +184,8 @@ const userName = computed(() => currentUserStore.getFullName);
           type="submit"
           severity="secondary"
           label="Dodaj firmę"
-          :loading="createCompanyWithAddressMutation.isPending.value"
-          :disabled="createCompanyWithAddressMutation.isPending.value"
+          :loading="createCompanyMutation.isPending.value"
+          :disabled="createCompanyMutation.isPending.value"
           class="sm:w-100"
         />
       </Form>
